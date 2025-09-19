593c640d00824e58d59855a19395d6db97e17315
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-09-19 22:20:25 +0530

    MVC pattern curd operation using Postman API latest

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-//import express module
-const mongoose=require('mongoose');
-const app = require('./app');
+    //import express module
+    const mongoose=require('mongoose');
+    const app = require('./app');
 
-require('dotenv').config();
-//create an express apllication
+    require('dotenv').config();
+    //create an express apllication
 
-mongoose.connect(process.env.MONGODB_URI)
-.then(()=>{
-    console.log("connect to MongoDB");
-    app.listen(8001,'localhost',()=>{
-    console.log(`server is running on http://localhost:8001`)
-});
+    mongoose.connect(process.env.MONGODB_URI)
+    .then(()=>{
+        console.log("connect to MongoDB");
+        app.listen(8001,'0.0.0.0',()=>{
+        console.log(`server is running on http://0.0.0.0:8001`)
+    });
 
 
-})
-.catch((err)=>console.log("could not connext"))
+    })
+    .catch((err)=>console.log("could not connext"))
