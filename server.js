c5bea710ee9ca24f13fcc31bc8ff56fed1d7f8ad
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-09-20 18:03:50 +0530

    render deploy port

--- server.js
+++ server.js
@@ -4,11 +4,11 @@
 
     require('dotenv').config();
     //create an express apllication
-
+    const PORT=process.env.PORT||3000;
     mongoose.connect(process.env.MONGODB_URI)
     .then(()=>{
         console.log("connect to MongoDB");
-        app.listen(8000,'localhost',()=>{
+        app.listen(PORT,'localhost',()=>{
         console.log(`server is running on http://localhost:8001`)
     });
 
